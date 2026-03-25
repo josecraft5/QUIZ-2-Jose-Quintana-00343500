d4b6510fe2453c397b6f90a8000ddb7669a540e8
Pc_escritorio_JoseQ <josegabrielquintana@gmail.com>
2026-03-24 20:21:20 -0500

    Al codigo le hice una optimizacion y cambie las variables para que se vea mas compacto

--- MAIN.py
+++ MAIN.py
@@ -1,19 +1,17 @@
 rango = int(input("Cuantos numeros deseas ingresar?: "))
 
-lista_original = []
+ls = []
 for i in range(1,rango+1):
     num = int(input(f"Ingrese el numero {i}: "))
-    lista_original.append(num)
+    ls.append(num)
 
-print(f"lista original: {lista_original}")
-for i in range(len(lista_original)-1):
-    if lista_original[i] > lista_original[i+1]:
-        lista_original[i], lista_original[i+1] = lista_original[i+1], lista_original[i]
-    for i2 in range(len(lista_original)-1):
-        if lista_original[i2] > lista_original[i2+1]:
-            lista_original[i2], lista_original[i2+1] = lista_original[i2+1], lista_original[i2]
-        
-print(f"lista ordenada: {lista_original}")
+print(f"lista original: {ls}")
+for i in range(len(ls)-1):
+    for i2 in range(len(ls)-1):
+        if ls[i2] > ls[i2+1]:
+            ls[i2], ls[i2+1] = ls[i2+1], ls[i2]
+
+print(f"lista ordenada: {ls}")
 
 
 
